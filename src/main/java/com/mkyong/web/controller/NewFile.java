b286accf8ef258a2bfe6707c24b36d3ff72c27f8
JIEM-L24\mayuri <mayuri.gore@jiem.in>
2018-07-05 15:56:46 +0530

    gffdh

--- src/main/java/com/mkyong/web/controller/NewFile.java
+++ src/main/java/com/mkyong/web/controller/NewFile.java
@@ -3,7 +3,8 @@ package com.mkyong.web.controller;
 public class NewFile {
 
 	public static void main(String[] args) {
-	System.out.println("First Git App");
+		System.out.println("My First Git Application : ");
+
 	}
 
 }

60b221d4e9b4e36358297cfed1841c0a2707c04a
JIEM-L24\mayuri <mayuri.gore@jiem.in>
2018-07-26 12:41:22 +0530

    Demo Test add data Mayuri

--- src/main/java/com/mkyong/web/controller/NewFile.java
+++ src/main/java/com/mkyong/web/controller/NewFile.java
@@ -5,6 +5,7 @@ public class NewFile {
 	public static void main(String[] args) {
 
 		System.out.println("My First Git Application :hello worls ");
+		System.out.println("My Second Git Application :hello worls ");
 
 
 	}
